5e8735770a61207b71cc628546bb08630bf95f97
Wei Da <wd312@cl.cam.ac.uk>
2024-12-20 18:26:36 +0000

    fix the experiment with bool para to decide whether update code

--- vidur/prediction/exp/experiment.sh
+++ vidur/prediction/exp/experiment.sh
@@ -10,9 +10,9 @@ M=12
 N=12
 HOST_CONFIG_PATH='vidur/prediction/config/host_configs.json'
 PREDICTOR_CONFIG_PATH="vidur/prediction/config/test_config.json"
-DISABLE_TIME_ESTIMATION="false"
-UPDATE_VIDUR_CODE= 'true'
-UPDATE_VLLM_CODE= 'false'
+DISABLE_TIME_ESTIMATION=false
+UPDATE_VIDUR_CODE=true
+UPDATE_VLLM_CODE=false
 
 case "$1" in
     -d|--daemon)
@@ -27,6 +27,6 @@ esac
 #rm -rf experiment_output
 #mkdir -p experiment_output/logs
 sh vidur/prediction/exp/reset.sh
-nohup sh vidur/prediction/exp/run_exp_vllm.sh $BATCH_CAP $MODEL $UPDATE_VIDUR_CODE> /dev/null 2>&1 &
+nohup sh vidur/prediction/exp/run_exp_vllm.sh $BATCH_CAP $MODEL $UPDATE_UPDATE_VLLM_CODE > /dev/null 2>&1 &
 nohup sh vidur/prediction/exp/run_exp_predictor.sh $PREDICTOR_CONFIG_PATH $METRIC_TYPE $DISABLE_TIME_ESTIMATION $UPDATE_VIDUR_CODE> /dev/null 2>&1 &
 nohup sh vidur/prediction/exp/run_exp_global_scheduler.sh $TARGET_HOST $M $N $METRIC_TYPE $HOST_CONFIG_PATH> /dev/null 2>&1 &
