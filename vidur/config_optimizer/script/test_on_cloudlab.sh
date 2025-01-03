c4d44512730c753822356ca2f1e8514547b6eb22
Wei Da <wd312@cl.cam.ac.uk>
2025-01-03 00:48:57 +0000

    improve simulation scripts

--- vidur/config_optimizer/script/test_on_cloudlab.sh
+++ vidur/config_optimizer/script/test_on_cloudlab.sh
@@ -1,11 +1,14 @@
-parallel-ssh -h vidur/config_optimizer/script/cl_host "sudo apt update && sudo apt install -y python3-pip"
-parallel-ssh -t 0 -h vidur/config_optimizer/script/cl_host "git clone https://github.com/AKafakA/vidur_opt_scheduler.git && cd vidur_opt_scheduler && git checkout exp && pip install -r requirements.txt"
-parallel-ssh -t 0 -h vidur/config_optimizer/script/cl_host "cd vidur_opt_scheduler && git pull"
-mapfile -t ary < <(sed 's/.*/"&"/' "./vidur/config_optimizer/script/cl_host")
+#parallel-ssh -i -h vidur/config_optimizer/script/cl_host "sudo apt update && sudo apt install -y python3-pip"
+#parallel-ssh -i -t 0 -h vidur/config_optimizer/script/cl_host "git clone https://github.com/AKafakA/vidur_opt_scheduler.git && cd vidur_opt_scheduler && git checkout exp && pip install -r requirements.txt"
+parallel-ssh -i -t 0 -h vidur/config_optimizer/script/cl_host "cd vidur_opt_scheduler && git pull"
+#parallel-ssh -t 0 -h vidur/config_optimizer/script/cl_host "pip install pyinstrument"
+mapfile -t ary < <(sed 's/.*/&/' "./vidur/config_optimizer/script/cl_host")
 
 counter=0
 for filename in vidur/config_optimizer/config_explorer/config/experiment_config/global_*.yml; do
-    parallel-ssh -t 0 --host ${ary[counter]} "cd vidur_opt_scheduler &&  export PYTHONPATH=. && nohup python vidur/config_optimizer/config_explorer/main.py --config-path ${filename} --num-threads 10 --output-dir ./simulation_output &"
+    echo "Running on ${ary[counter]}"
+    parallel-ssh -i --host ${ary[counter]} "pkill -f vidur & rm -rf vidur_opt_scheduler/simulation_output"
+    parallel-ssh --host ${ary[counter]} "cd vidur_opt_scheduler && nohup sh vidur/config_optimizer/script/run_simulation.sh ${filename} &"
     counter=$((counter+1))
 done
 
