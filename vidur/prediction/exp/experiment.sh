95beb8ae4199a1f6637394d19a87b30b31496e0e
Wei Da <wd312@cl.cam.ac.uk>
2025-01-04 16:25:06 +0000

    fix the dataset link

--- vidur/prediction/exp/experiment.sh
+++ vidur/prediction/exp/experiment.sh
@@ -15,10 +15,10 @@ DATASET_PATH="~/$DATASET_NAME.json"
 DATASET_TYPE="sharegpt"
 
 GENERATE_NEW_DATA=false
-DOWNLOAD_DATASET=false
+DOWNLOAD_DATASET=true
 UPDATE_VIDUR_CODE=true
 UPDATE_VLLM_CODE=false
-RESTART_VLLM=false
+RESTART_VLLM=true
 RUN_EXP=true
 
 case "$1" in
@@ -46,7 +46,7 @@ if [ "$RUN_EXP" = "true" ]; then
   NUM_QUERIES="10000"
   METRIC_TYPES="random min_latency round_robin min_current_gpu_blocks min_pending_requests min_gpu_blocks"
   if [ "$DOWNLOAD_DATASET" = "true" ]; then
-    parallel-ssh -t 0 --host $TARGET_HOST "wget https://github.com/AKafakA/Sequence-Scheduling/blob/main/data/sharegpt-val-10k-predicted.json"
+    parallel-ssh -t 0 --host $TARGET_HOST "wget https://huggingface.co/datasets/asdwb/sharegpt_length_prediction/resolve/main/$DATASET_NAME.json"
   fi
   for qps in $QPS; do
       for num_queries in $NUM_QUERIES; do
@@ -57,9 +57,8 @@ if [ "$RUN_EXP" = "true" ]; then
               N="12"
           fi
           for n in $N; do
-                  parallel-ssh -t 0 --host $TARGET_HOST "pkill -f predictor"
-                  nohup sh vidur/prediction/exp/run_exp_predictor.sh $PREDICTOR_CONFIG_PATH $METRIC_TYPE $DISABLE_TIME_ESTIMATION $UPDATE_VIDUR_CODE $BATCH_CAP> /dev/null 2>&1 &
                   echo "Running experiment with qps: $qps, num_queries: $num_queries, n: $n, metric_type: $metric_type"
+                  nohup sh vidur/prediction/exp/run_exp_predictor.sh $PREDICTOR_CONFIG_PATH $METRIC_TYPE $DISABLE_TIME_ESTIMATION $UPDATE_VIDUR_CODE $BATCH_CAP> /dev/null 2>&1 &
                   nohup sh vidur/prediction/exp/run_exp_global_scheduler.sh $TARGET_HOST $n $n $metric_type $HOST_CONFIG_PATH > /dev/null 2>&1 &
                   LOG_FILENAME="benchmark.log"
                   OUTPUT_DIR="${metric_type}/qps_${qps}_num_queries_${num_queries}_n_${n}"
@@ -73,6 +72,7 @@ if [ "$RUN_EXP" = "true" ]; then
   #  test if using the estimated length
   metric_type="min_latency min_gpu_blocks"
   N="2 6 12"
+  for metric_type in $METRIC_TYPES; do
     for qps in $QPS; do
       for num_queries in $NUM_QUERIES; do
         for n in $N; do
@@ -83,9 +83,10 @@ if [ "$RUN_EXP" = "true" ]; then
          OUTPUT_DIR="${metric_type}*/qps_${qps}_num_queries_${num_queries}_n_${n}_estimated_length"
          parallel-ssh -i -t 0 --host $TARGET_HOST "cd vidur_opt_scheduler && export PYTHONPATH=. && python vidur/prediction/benchmark/benchmark_serving.py --ip_ports 127.0.0.1:8200 --tokenizer $MODEL --num_sampled_requests $num_queries --dataset_type $DATASET_TYPE --dataset_path $DATASET_PATH --qps $qps --backend block --log_filename $LOG_FILENAME --output_dir $OUTPUT_DIR --tag_dataset_with_real_response $GENERATE_NEW_DATA --enable_csv_files $GENERATE_NEW_DATA --use_estimated_length true"
          sleep 10
-         done
+        done
       done
     done
+  done
 fi
 
 
--- vidur/prediction/exp/run_exp_global_scheduler.sh
+++ vidur/prediction/exp/run_exp_global_scheduler.sh
@@ -5,5 +5,5 @@ METRICS_TYPE=$4
 CONFIG_PATH=$5
 
 parallel-ssh -t 0 --host $HOST "pkill -f global_scheduler"
-sleep 60
+sleep 10
 parallel-ssh -t 0 --host $HOST "cd vidur_opt_scheduler && export PYTHONPATH=. && nohup python vidur/prediction/global_scheduler/api_server.py --config_path $CONFIG_PATH --metrics_type $METRICS_TYPE --num_query_predictor $M --num_required_predictor $N > experiment_output/logs/global_scheduler.log 2>&1 &"
