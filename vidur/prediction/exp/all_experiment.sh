783a4b728d21f9f3b5d9ed2ab9df2075c52f5a98
Wei Da <wd312@cl.cam.ac.uk>
2025-01-04 22:02:36 +0000

    fix all exp

--- vidur/prediction/exp/all_experiment.sh
+++ vidur/prediction/exp/all_experiment.sh
@@ -1,5 +1,5 @@
-SCHEDULER_NAME = "min_latency min_current_gpu_blocks min_current_requests min_gpu_blocks"
-BATCH_CAP = 48
+SCHEDULER_NAME="min_latency min_current_gpu_blocks min_current_requests min_gpu_blocks"
+BATCH_CAP=48
 
 for scheduler in $SCHEDULER_NAME; do
   echo "Running experiment for scheduler: $scheduler"
