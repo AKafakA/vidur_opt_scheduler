bb96bdacf5a8defb1795724a8b01ead5712e9dd7
Wei Da <wd312@cl.cam.ac.uk>
2025-05-17 22:10:30 +0100

    use mutiprocess for prediction

--- vidur/prediction/predictor/simulate_predictor.py
+++ vidur/prediction/predictor/simulate_predictor.py
@@ -1,6 +1,7 @@
 import multiprocessing
 import random
 import time
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 from math import ceil
 import itertools
 import logging
@@ -90,6 +91,7 @@ class SimulatePredictor(Predictor):
         self._start_time = time.time()
         self._backend_url = f"http://localhost:{self._port}/schedule_trace"
         self._query_timeout = aiohttp.ClientTimeout(total=config.prediction_timeout * 0.9)
+        self._executor = ProcessPoolExecutor(max_workers=14)
 
     async def predict(self, target_request: Request):
         start_time = time.time()
@@ -100,8 +102,16 @@ class SimulatePredictor(Predictor):
         # replica_scheduler.print_requests()
         if self._need_to_predict:
             start_predict = time.time()
-            target_metric_future = asyncio.ensure_future(self.get_predicted_metrics(response_data, target_request))
-            target_metric = await target_metric_future
+            # target_metric_future = asyncio.ensure_future(self.get_predicted_metrics(response_data, target_request))
+            # target_metric = await target_metric_future
+            loop = asyncio.get_event_loop()
+            with ProcessPoolExecutor(max_workers=10) as executor:
+                target_metric = await loop.run_in_executor(
+                    executor,
+                    self.get_predicted_metrics,
+                    response_data,
+                    target_request
+                )
             print(f"simulation taking {(time.time() - start_predict) * 1000} ms")
         elif self._config.target_metric == "min_current_gpu_blocks":
             target_metric = response_data["free_gpu_blocks"]
