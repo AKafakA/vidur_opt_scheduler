f50af2ff2f3b552214e9b3ab6a2403fdb0cd519c
Wei Da <wd312@cl.cam.ac.uk>
2025-05-18 23:06:37 +0100

    add new profile middware

--- vidur/prediction/exp/experiment.sh
+++ vidur/prediction/exp/experiment.sh
@@ -55,6 +55,8 @@ case "$1" in
         ;;
 esac
 
+parallel-ssh -i -t 0 -h vidur/prediction/config/hosts "pip install fastapi-cprofile"
+
 
 #rm -rf experiment_output
 #mkdir -p experiment_output/logs
