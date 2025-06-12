a865e30ee8f8756c73ed1d76c5cba4b0507f8ca7
Wei Da <wd312@cl.cam.ac.uk>
2025-06-12 15:09:32 +0100

    fix the name of qwen to qwen2

--- vidur/prediction/exp/end_to_end_exp_scripts/extension_experiment.sh
+++ vidur/prediction/exp/end_to_end_exp_scripts/extension_experiment.sh
@@ -1,7 +1,7 @@
 START_INDEX=0
 BATCH_CAP=48
 TARGET_HOST='asdwb@d7525-10s10309.wisc.cloudlab.us'
-PREDICTOR_WORKERS=16
+PREDICTOR_WORKERS=1
 GLOBAL_SCHEDULER_WORKERS=1
 BACKEND_WORKERS=1
 MAX_MODEL_LENGTH=4096
@@ -18,13 +18,13 @@ RESTART_VLLM=true
 
 ENABLE_CHUNKED_PREFILL="true"
 
-MODEL="meta-llama/Llama-2-7b-hf"
+MODEL="Qwen/Qwen2-7B"
 SCHEDULER_NAME="min_new_request_latency"
 #QPS="30 24 18"
-QPS="30 15 60 22.5 45"
+QPS="10"
 PROFILING_SAMPLE_RATE=0.0
 USE_FOR_PROFILING_ONLY=false
-NUM_REQUEST=10000
+NUM_REQUEST=100
 KEEP_ALL_METRICS=false
 N_SELECTED="12"
 OUTPUT_DIR_PREFIX="extension"
@@ -41,7 +41,7 @@ for model in $MODEL; do
   for dataset_name in $DATASET_NAMES; do
     for scheduler in $SCHEDULER_NAME; do
       if [ "$scheduler" = "min_new_request_latency" ]; then
-        USE_LENGTH_ESTIMATION="false"
+        USE_LENGTH_ESTIMATION="true"
       else
         USE_LENGTH_ESTIMATION="false"
       fi
