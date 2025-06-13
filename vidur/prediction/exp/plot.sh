9f7ba7a42c8514c5c83cecaa2df0a7a158fc7534
Wei Da <wd312@cl.cam.ac.uk>
2025-06-13 17:11:27 +0100

    experiment plot improvement

--- vidur/prediction/exp/plot.sh
+++ vidur/prediction/exp/plot.sh
@@ -9,20 +9,11 @@ python3 experiments_analysis/experiment_plot.py \
     --max-qps 36 \
     --min-qps 20 \
     --num-of-cdf-figures 5 \
-    --zoomed True
+    --zoomed
 
 python3 experiments_analysis/prediction_plot.py \
     --experiments-dir experiment_output/data/prediction/sharegpt \
     --output-dir experiment_output/results/prediction \
-
-#python3 experiments_analysis/experiment_plot.py \
-#    --experiments-dir experiment_output/config_search/sharegpt \
-#    --output-dir experiment_output/results/main \
-#    --ttft-p99-slo $TTFT_SLO \
-#    --max-qps 36 \
-#    --min-qps 20 \
-#    --num-of-cdf-figures 5 \
-#    --zoomed True
 
 python3 experiments_analysis/experiment_plot.py \
     --experiments-dir experiment_output/data/extension/burstgpt \
@@ -31,15 +22,15 @@ python3 experiments_analysis/experiment_plot.py \
     --max-qps 60 \
     --min-qps 55 \
     --num-of-cdf-figures 5 \
-    --zoomed True
 
 python3 experiments_analysis/experiment_plot.py \
     --experiments-dir experiment_output/data/extension/sharegpt \
     --output-dir experiment_output/results/qwen \
     --ttft-p99-slo $TTFT_SLO \
     --max-qps 70 \
-    --min-qps 50 \
+    --min-qps 55 \
     --num-of-cdf-figures 5 \
-    --zoomed True
 
-git checkout -b
+python3 experiments_analysis/auto_provision_plot.py \
+    --experiments-dir experiment_output/data/auto_provision/sharegpt \
+    --output-dir experiment_output/results/auto_provision
