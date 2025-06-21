5dfa01de2525bd54bdb8e11483ebeec105c6a6dd
Wei Da <wd312@cl.cam.ac.uk>
2025-06-21 17:49:58 +0100

    add scheduling overhead in the qps plot

--- experiments_analysis/auto_provision_plot.py
+++ experiments_analysis/auto_provision_plot.py
@@ -47,27 +47,24 @@ def extract_num_available_instances_plot(num_instances_list, min_instances=6):
     return num_instance_curve
 
 
-def plot_dual_timeline_data(experiments_set, latency_ax, gpu_free_ax, gpu_variance_ax, instance_ax, font_size=10,
+def plot_dual_timeline_data(experiments_set, latency_ax, gpu_free_ax, gpu_variance_ax, instance_ax, font_size=12,
                             provisioning_threshold=60, sigma=10):
     """
     Plot two sets of data on the same timeline with different y-axes.
     """
-    initial_text_height = 90
-    latency_ax.text(5000, initial_text_height, "Num over Cond", fontsize=font_size, verticalalignment='bottom')
-    latency_ax.text(5500, initial_text_height + 10, "And", fontsize=font_size, verticalalignment='bottom')
-    latency_ax.text(5250, initial_text_height + 20, "P99 Latency(s)", fontsize=font_size, verticalalignment='bottom')
+    initial_text_height = 80
     text_info = {}
     for exp in experiments_set:
         enable_preemptive_provision = exp['enable_preemptive_provision']
         enable_auto_scaling = exp['enable_auto_scaling']
         if enable_preemptive_provision:
-            label1 = "Preemptive Provision"
+            label1 = "Preempt"
             color1 = "blue"
         elif enable_auto_scaling:
-            label1 = "Relief Provision"
+            label1 = "Relief"
             color1 = "orange"
         else:
-            label1 = "Static Provisioned"
+            label1 = "Static"
             color1 = "green"
         latencies = exp['request_latencies']
         num_request_with_latencies_more_than_threshold = np.sum(latencies >= provisioning_threshold)
@@ -81,14 +78,14 @@ def plot_dual_timeline_data(experiments_set, latency_ax, gpu_free_ax, gpu_varian
         x = np.arange(len(latencies))
         # ttft = gaussian_filter1d(ttft, sigma=5)
         # ax1.plot(x, ttft, label=label1, color=color1, linewidth=2)
-        latency_ax.scatter(x, latencies, label=label1, color=color1, s=0.1)
+        latency_ax.scatter(x, latencies, label=label1, color=color1, s=1)
         p99_latency = np.percentile(latencies, 99)
         latency_ax.fill_between(x, latencies, color=color1, alpha=0.1)
 
         latency_ax.set_ylabel("Latency (s)", fontsize=font_size)
         instance_ax.scatter(x, available_instances, label=label1, color=color1, s=0.01)
         instance_ax.set_ylabel("Available Instances", fontsize=font_size)
-        instance_ax.set_xlabel("Request Index", fontsize=font_size)
+        instance_ax.set_xlabel("Query ID", fontsize=font_size)
 
         num_instance_curve = extract_num_available_instances_plot(available_instances, min_instances=6)
         instance_ax.plot(x, num_instance_curve, label=label1, color=color1, linewidth=0.8, linestyle='--')
@@ -98,18 +95,22 @@ def plot_dual_timeline_data(experiments_set, latency_ax, gpu_free_ax, gpu_varian
         gpu_variance_ax.plot(x, smoothed_var_gpu_blocks, label=label1, color=color1, linewidth=2)
         gpu_variance_ax.set_ylabel("Var GPU Blocks", fontsize=font_size)
 
-        gpu_variance_ax.set_xlabel("Request Index", fontsize=font_size)
+        gpu_variance_ax.set_xlabel("Query ID", fontsize=font_size)
 
         latency_ax.plot([0, len(experiments_set[0]['request_latencies'])], [provisioning_threshold, provisioning_threshold],
                  color='red', linewidth=1)
-        latency_ax.text(0, provisioning_threshold + 10, "Scaling Cond", color='red', fontsize=font_size,
+
+        latency_ax.text(0, provisioning_threshold + 10, "Scaling SLO", color='red', fontsize=font_size,
                         verticalalignment='bottom')
-        latency_ax.legend(loc='upper left', fontsize=font_size, ncol=3, bbox_to_anchor=(0, 1.25))
-        gpu_free_ax.legend(loc='upper left', fontsize=font_size, ncol=3, bbox_to_anchor=(0, 1.25))
+
+        latency_ax.legend(loc='upper left', fontsize=font_size, ncol=3, bbox_to_anchor=(0.2, 1.25),
+                          scatterpoints=1, markerscale=10)
+        gpu_free_ax.legend(loc='upper left', fontsize=font_size, ncol=3, bbox_to_anchor=(0.2, 1.25),
+                           fancybox=False, shadow=False)
 
         text_info[label1] = (num_request_with_latencies_more_than_threshold, p99_latency, color1)
 
-    text_sort = ["Static Provisioned", "Preemptive Provision", "Relief Provision"]
+    text_sort = ["Relief", "Preempt", "Static",]
     text_sort.reverse()
     for label in text_sort:
         num_request_with_latencies_more_than_threshold, p99_latency, color = text_info[label]
@@ -117,6 +118,7 @@ def plot_dual_timeline_data(experiments_set, latency_ax, gpu_free_ax, gpu_varian
                         f"{label}:{p99_latency:.1f}, {num_request_with_latencies_more_than_threshold}",
                         color=color, fontsize=font_size, verticalalignment='bottom')
         initial_text_height += 10
+    latency_ax.text(6000, initial_text_height, "Scheduler:(P99 lat, Num>SLO)", fontsize=font_size, verticalalignment='bottom')
 
 
 def plot_per_qps(experiments_set, output_dir, selected_qps, provisioning_threshold=60):
