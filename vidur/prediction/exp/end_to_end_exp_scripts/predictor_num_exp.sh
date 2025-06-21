4ab69cd5bc60328755566b52121b49e529b85565
Wei Da <wd312@cl.cam.ac.uk>
2025-06-21 21:26:16 +0100

    fix typo in all exp and add predictor work exp to check it effectness

--- vidur/prediction/exp/end_to_end_exp_scripts/predictor_num_exp.sh
+++ vidur/prediction/exp/end_to_end_exp_scripts/predictor_num_exp.sh
@@ -1,7 +1,7 @@
 START_INDEX=0
 BATCH_CAP=48
 TARGET_HOST='asdwb@d7525-10s10309.wisc.cloudlab.us'
-PREDICTOR_WORKERS=16
+PREDICTOR_WORKERS="1 8 16"
 GLOBAL_SCHEDULER_WORKERS=1
 BACKEND_WORKERS=1
 MAX_MODEL_LENGTH=4096
@@ -20,7 +20,7 @@ RESTART_VLLM=true
 ENABLE_CHUNKED_PREFILL="true"
 MODEL="meta-llama/Llama-2-7b-hf"
 DATASET_NAMES="sharegpt"
-SCHEDULER_NAME="min_new_request_latency min_lunmnix_load min_infass_load round_robin request_per_seconds random"
+SCHEDULER_NAME="min_new_request_latency round_robin request_per_seconds min_infass_load"
 PROFILING_SAMPLE_RATE=0.000
 QPS="20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36"
 USE_FOR_PROFILING_ONLY=false
@@ -56,9 +56,11 @@ for model in $MODEL; do
           for batch_size_cut in $BATCH_SIZE_THRESHOLD_FOR_TIME_ESTIMATION; do
             for n_selected in $N_SELECTED; do
               for qps in $QPS; do
-                dataset_path="~/vidur_opt_scheduler/data/trace_data/$dataset_name/generate/$MODEL_TYPE"
-                echo "Running experiment with scheduler: $scheduler, model: $model, dataset: $dataset_name, qps: $qps, batch_size_cut: $batch_size_cut enable_chunked_prefill: $enable_chunked_prefill use_for_profiling_only: $USE_FOR_PROFILING_ONLY predictor timeout: $PREDICTOR_TIMEOUT_IN_SECONDS"
-                sh vidur/prediction/exp/experiment.sh $scheduler $NUM_REQUEST $RESTART_VLLM  $BATCH_CAP $dataset_name $dataset_path $dataset_name true $KEEP_ALL_METRICS $START_INDEX $model $MODEL_TYPE $MAX_MODEL_LENGTH $TARGET_HOST $enable_chunked_prefill $PREDICTOR_WORKERS $GLOBAL_SCHEDULER_WORKERS $BACKEND_WORKERS $CHUNK_SIZE $qps $BRANCH_NAME $batch_size_cut $n_selected $PROFILING_SAMPLE_RATE $TIMEOUT_IN_SECONDS $USE_FOR_PROFILING_ONLY $PREDICTOR_TIMEOUT_IN_SECONDS $USE_PROCESS_FOR_FRONTEND $UPDATE_VIDUR_CODE $UPDATE_VLLM_CODE $RUN_EXP $use_estimation_len $OUTPUT_DIR_PREFIX $AVAILABLE_INSTANCE $MAX_SLO $ENABLE_PREEMPTIVE_AUTO_PROVISIONING
+                for predictor_workers in $PREDICTOR_WORKERS; do
+                  dataset_path="~/vidur_opt_scheduler/data/trace_data/$dataset_name/generate/$MODEL_TYPE"
+                  echo "Running experiment with scheduler: $scheduler, model: $model, dataset: $dataset_name, qps: $qps, batch_size_cut: $batch_size_cut enable_chunked_prefill: $enable_chunked_prefill use_for_profiling_only: $USE_FOR_PROFILING_ONLY predictor timeout: $PREDICTOR_TIMEOUT_IN_SECONDS predictor workers: $predictor_workers"
+                  sh vidur/prediction/exp/experiment.sh $scheduler $NUM_REQUEST $RESTART_VLLM  $BATCH_CAP $dataset_name $dataset_path $dataset_name true $KEEP_ALL_METRICS $START_INDEX $model $MODEL_TYPE $MAX_MODEL_LENGTH $TARGET_HOST $enable_chunked_prefill $predictor_workers $GLOBAL_SCHEDULER_WORKERS $BACKEND_WORKERS $CHUNK_SIZE $qps $BRANCH_NAME $batch_size_cut $n_selected $PROFILING_SAMPLE_RATE $TIMEOUT_IN_SECONDS $USE_FOR_PROFILING_ONLY $PREDICTOR_TIMEOUT_IN_SECONDS $USE_PROCESS_FOR_FRONTEND $UPDATE_VIDUR_CODE $UPDATE_VLLM_CODE $RUN_EXP $use_estimation_len $OUTPUT_DIR_PREFIX $AVAILABLE_INSTANCE $MAX_SLO $ENABLE_PREEMPTIVE_AUTO_PROVISIONING
+                done
               done
             done
           done
